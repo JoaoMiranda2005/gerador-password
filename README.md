e35716a8422cb7f9588a1ac4c78dc9bc7afff423
João Miranda <88510677+JoaoMiranda2005@users.noreply.github.com>
2021-09-11 14:51:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gerador-password
+ um gerador de password simples 
---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+while True:
+  decision = input("Queres gerar uma password? y/n ")
+  if decision == 'y':
+    length = int(input("Enter the length of the password :" ))
+    print ("A Gerar Password...")
+    s="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    p = ''
+    for i in range(0, length, 1):
+      p += s[random.randint(0,len(s))]
+    time.sleep(3)
+    print(p)
+  else:
+     break 
